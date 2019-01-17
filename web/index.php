b991538205f25512e07d8cffc44571569a7ea359
proton5 <jessehillman7@hotmail.com>
2019-01-17 13:54:18 -0700

    homepage changes

--- web/index.php
+++ web/index.php
@@ -10,13 +10,13 @@ echo "
     <script>
     
     </script>
-</head>
+</head> ";
 
-<body>
+echo "<body>";
 
     
 
-    <?php
+
     echo "<nav>
         <ul>
         <li><a href="index.html">Home</a></li>
@@ -24,24 +24,25 @@ echo "
       </ul>
     </nav>";
 
-    ?>
 
 
+echo "
     <div class="container" id="firstContainer">
         <h1>Jesse Hillman</h1>
     </div>
+";
 
-
+echo "
     <div class="container">
         <p>Snowboarding, skateboarding, rockclimbing, and coding.</p>
         <img src="photo/profile.jpg" alt="snowboarding photo">
 
     </div>
-
-
-</body>
-
-</html>
 ";
+
+echo "</body>";
+
+echo "</html>";
+
 
 ?>
