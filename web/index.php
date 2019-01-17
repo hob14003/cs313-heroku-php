267eabe4934197f0984de05c851cd4cb03d911fd
proton5 <jessehillman7@hotmail.com>
2019-01-17 14:05:54 -0700

    fixing homepage

--- web/index.php
+++ web/index.php
@@ -6,42 +6,40 @@ echo "<Title> Jesse Hillman - Aspiring Developer </Title>";
 
 echo  "<link rel=\"stylesheet\" href=\"styles/style.css\">";
 
-//     <script>
+echo "     <script>
     
-//     </script>
-// </head> ";
+     </script>
+ </head> ";
 
-//  echo "<body>";
+  echo "<body>";
 
     
 
 
-//      echo "<nav>
-//         <ul>
-//         <li><a href="index.html">Home</a></li>
-//         <li><a href="assignments.html">Assignments</a></li>
-//       </ul>
-//     </nav>";
+      echo "<nav>
+         <ul>
+         <li><a href=\"index.html\">Home</a></li>
+         <li><a href=\"assignments.html\">Assignments</a></li>
+       </ul>
+     </nav>";
 
 
 
-// echo "
-//     <div class="container" id="firstContainer">
-//         <h1>Jesse Hillman</h1>
-//     </div>
-// ";
+ echo "
+     <div class=\"container\" id=\"firstContainer\">
+         <h1>Jesse Hillman</h1>
+     </div>
+ ";
 
-// echo "
-//     <div class="container">
-//         <p>Snowboarding, skateboarding, rockclimbing, and coding.</p>
-//         <img src="photo/profile.jpg" alt="snowboarding photo">
+ echo "
+     <div class=\"container\">
+         <p>Snowboarding, skateboarding, rockclimbing, and coding.</p>
+         <img src=\"photo/profile.jpg\" alt=\"snowboarding photo\">
 
-//     </div>
-// ";
+     </div>";
 
-// echo "</body>";
-
-// echo "</html>";
+ echo "</body>";
+ echo "</html>";
 
 
 ?>
