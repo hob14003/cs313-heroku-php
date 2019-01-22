fe44f043e99dbe7814939d96d6b91dd86a84ec49
proton5 <jessehillman7@hotmail.com>
2019-01-22 13:39:01 -0700

    added team activity 2

--- web/team-activity2/process.php
+++ web/team-activity2/process.php
@@ -4,6 +4,10 @@
 Welcome <?php echo $_POST["name"]; ?><br>
 Your email address is: <?php echo $_POST["email"]; ?>
 
+Your major is: <?php echo $_POST["choices"]; ?>
+
+Comments: <?php echo $_POST["summary"]; ?>
+
 
 </body>
 </html>
