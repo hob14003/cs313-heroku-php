ce0e1305418fe773e23c14857b762b4c6f273c30
proton5 <jessehillman7@hotmail.com>
2019-01-22 14:12:40 -0700

    added team activity 2

--- web/team-activity2/process.php
+++ web/team-activity2/process.php
@@ -13,6 +13,13 @@ Comments: <?php echo $_POST["summary"]; ?><br>
 
 Continents we've visited: 
 <?php if( isset($_POST['northA']) ) { echo $_POST['northA']; } ?><br>
+<?php if( isset($_POST['southA']) ) { echo $_POST['southA']; } ?><br>
+<?php if( isset($_POST['euro']) ) { echo $_POST['euro']; } ?><br>
+<?php if( isset($_POST['asia']) ) { echo $_POST['asia']; } ?><br>
+<?php if( isset($_POST['australia']) ) { echo $_POST['australia']; } ?><br>
+<?php if( isset($_POST['africa']) ) { echo $_POST['africa']; } ?><br>
+<?php if( isset($_POST['antartica']) ) { echo $_POST['antartica']; } ?><br>
+
 
 </body>
 </html>
