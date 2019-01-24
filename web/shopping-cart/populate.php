f5fddc0cba115b6d63e9f4babe5e38022d36b1ed
proton5 <jessehillman7@hotmail.com>
2019-01-24 13:23:44 -0700

    adding shopping cart home page

--- web/shopping-cart/populate.php
+++ web/shopping-cart/populate.php
@@ -7,6 +7,7 @@ echo "<nav>";
 foreach($navigation as $navItem)
 {
   echo  "<ul><li><a href=\"$navItem.php\">$navItem</a></li></ul>";
+  
 }
 
 echo "</nav>";
--- web/shopping-cart/storage.php
+++ web/shopping-cart/storage.php
@@ -1,8 +1,9 @@
 <?php
 
+// navigation bar
 $navigation = array("Browse","View Cart", "Checkout");
 
-$test = "testing";
+
 // keep a record of all the items to browse
 $skateboardDeck = array("red","blue","yellow");
 $toques = array("red","blue","yellow");
