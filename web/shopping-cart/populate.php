9e070e8972698d677a775fc5063ee4ba1a20cc51
proton5 <jessehillman7@hotmail.com>
2019-01-24 13:29:10 -0700

    adding shopping cart home page

--- web/shopping-cart/populate.php
+++ web/shopping-cart/populate.php
@@ -5,10 +5,9 @@ echo "<nav>";
 
 
 foreach($navigation as $navItem)
-{
-  echo  "<ul><li><a href=\"$navItem.php\">$navItem</a></li></ul>";
-  
-}
+echo  "<ul><li><a href=\"$navItem.php\">$navItem</a></li></ul>";
+
+
 
 echo "</nav>";
 
