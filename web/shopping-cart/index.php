412d8193cb86c045118157dcf984d0941577d6c5
proton5 <jessehillman7@hotmail.com>
2019-01-24 13:05:38 -0700

    adding shopping cart home page

--- web/shopping-cart/index.php
+++ web/shopping-cart/index.php
@@ -9,7 +9,17 @@
 
         <div class="container">
          <form>
-             <?php include "storage.php"; include "populate.php"; showItems(); ?>
+             <?php include "storage.php";      
+             
+    foreach($skateboardDeck as $deck)
+    {
+        echo "<p> hello world </p>";
+        echo "<input type=\"radio\" name=\"deck\" value=\"$deck skate deck\"> $skateboardDeck[0] <br>";
+    }
+    
+    
+    
+    ?>
               
          </form>
         </div>
