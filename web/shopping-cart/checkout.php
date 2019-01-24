abe0775a04577fb4f94458d99c5ab77596739a13
proton5 <jessehillman7@hotmail.com>
2019-01-24 16:27:15 -0700

    different approach

---
+++ web/shopping-cart/checkout.php
@@ -0,0 +1,22 @@
+<?php session_start(); ?>
+
+<html>
+
+<head>
+<link rel="stylesheet" href="style.css">
+
+
+</head>
+
+
+<body>
+    <!-- adds the navigation bar -->
+    <?php include "populate.php" ?>
+
+    
+</body>
+
+
+
+
+</html>
