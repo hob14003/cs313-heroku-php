f2eed76146933ba5911c39042402a55f69296e41
proton5 <jessehillman7@hotmail.com>
2019-01-24 11:09:12 -0700

    adding shopping cart home page

---
+++ web/shopping-cart/shop.php
@@ -0,0 +1,10 @@
+<?php
+
+// keep a record of all the items to browse
+$skateboardDeck = array("red","blue","yellow");
+$toques = array("red","blue","yellow");
+$items = array($skateboardDeck,$toques);
+
+
+
+?>
