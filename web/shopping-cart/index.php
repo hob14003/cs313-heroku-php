844b17c3c0e93e43912ccc593a9d567ca3e181ae
proton5 <jessehillman7@hotmail.com>
2019-01-24 13:10:13 -0700

    adding shopping cart home page

--- web/shopping-cart/index.php
+++ web/shopping-cart/index.php
@@ -13,7 +13,7 @@
              
     foreach($skateboardDeck as $deck)
     {
-        echo "<input type=\"radio\" name=\"deck\" value=\"$deck skate deck\"> $skateboardDeck[0] <br>";
+        echo "<input type=\"radio\" name=\"deck\" value=\"$deck skate deck\"> $deck <br>";
     }
     
  
