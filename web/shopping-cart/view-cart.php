12089644785f3c04804ee20355d5da371e05f710
proton5 <jessehillman7@hotmail.com>
2019-01-24 14:34:52 -0700

    adding shopping cart home page

--- web/shopping-cart/view-cart.php
+++ web/shopping-cart/view-cart.php
@@ -4,24 +4,33 @@
 
 $cartItems = array();
 
-
+// for each purchased item sent from browse
+// add them to the cart.
 foreach($_POST["purchasedItems"] as $item)
 {
 //echo $item;
 array_push($cartItems,$item);
 }
 
-$_SESSION["cartItems"] = $cartItems;
-
-array_push($_SESSION["cartItems"], "hello world");
-
-foreach ($_SESSION["cartItems"] as $sitem)
+// check if there is a cart already
+// if there is
+if (array_key_exists('cartItems',$_SESSION) && !empty($_SESSION['cartItems']))
 {
-    echo $sitem;
+    // add each new item into the session cart
+    foreach($cartItems as $possiblePurchases)
+    {
+        array_push($_SESSION["cartItems"], $possiblePurchases);
+    }
+
+}
+else
+{
+    // if there isn't a cart just make a new one with
+    // the provided items from browse.
+    $_SESSION["cartItems"] = $cartItems;
 }
 
-
-$_SESSION["cartItems"] = $cartItems;
+// show me what I got
 print_r($_SESSION);
 
 ?>
