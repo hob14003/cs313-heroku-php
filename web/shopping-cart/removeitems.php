66487dd212c7c017b7405c42ec3b4dc8f2bc2c6c
proton5 <jessehillman7@hotmail.com>
2019-01-24 18:08:21 -0700

    different approach

--- web/shopping-cart/removeitems.php
+++ web/shopping-cart/removeitems.php
@@ -4,54 +4,19 @@
 
 $removedItems = array();
 
-echo "sklajdlsajkdajl";
 
 
 foreach($_POST['rejecteditems'] as $yui)
 {
     array_push($removedItems,$yui);
-
-echo $yui;
-//echo $item;
+    echo $yui;
 }
 
-
-
-// check if there is a cart already
-// if there is
-if (array_key_exists('cartItems',$_SESSION) && !empty($_SESSION['cartItems']))
+foreach($removedItems as $it)
 {
-
-
-    $trueCartItems = $_SESSION['cartItems'];
-
-    foreach($removedItems as $remove)
-    {
-        // Remove each new item from the session cart
-        foreach($trueCartItems as $possiblePurchases)
-        {
-            if ($remove == $possiblePurchases)
-            {
-                unset($trueCartItems[$possiblePurchases]);
-            }
-
-        }
-
-    }
-
-    // set the new session key
-    $_SESSION['cartItems'] = $trueCartItems;
-
-
-    
-    foreach($trueCartItems)
-    {
-        echo "1";
-    }
-
-
-    print_r($_SESSION);
+    echo $it;
 }
+
 
 
 
