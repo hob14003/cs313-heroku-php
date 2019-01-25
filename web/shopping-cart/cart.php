b5578f31de700bd196ebe91c11da9ed51cc2adb9
proton5 <jessehillman7@hotmail.com>
2019-01-24 17:36:01 -0700

    different approach

--- web/shopping-cart/cart.php
+++ web/shopping-cart/cart.php
@@ -14,24 +14,7 @@
 
 <?php
  
-        $removeMe = array();
-        
-        // remove requested items from cart.
-        foreach($_POST["rejecteditems"] as $item)
-        {
-            echo "<p> $item </p>";
-        //echo $item;
-        array_push($removeMe,$item);
-
-        }
-        
-        
-        foreach($removeMe as $removed)
-        {
-            echo $removed;
-            echo " <p> hello world </p>";
-        }
-
+include "removeitems.php";
 
 
         
