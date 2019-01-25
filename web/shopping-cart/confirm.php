8826fb1f2074e6a2d2414db63ff0be4af4317c47
proton5 <jessehillman7@hotmail.com>
2019-01-24 19:05:39 -0700

    different approach

---
+++ web/shopping-cart/confirm.php
@@ -0,0 +1,24 @@
+<?php session_start(); ?>
+<html>
+
+<head>
+<link rel="stylesheet" href="style.css">
+
+
+</head>
+
+
+<body>
+    <!-- adds the navigation bar -->
+    <?php include "populate.php" ?>
+
+
+    <div class="buffer">
+    <h1>Confirmation Page</h1>    
+    </div>    
+
+</body>
+
+
+
+</html>
