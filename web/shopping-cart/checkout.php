9c688830253d29862ad1cd3ddaf6446a695ed94c
proton5 <jessehillman7@hotmail.com>
2019-01-24 19:32:28 -0700

    different approach

--- web/shopping-cart/checkout.php
+++ web/shopping-cart/checkout.php
@@ -16,22 +16,21 @@
 
     <div class="buffer">
     <h1>Enter shipping information</h1>        
-</div>
+    </div>
 
     <div class="container">
     <form action="confirm.php" action="post">
 
-    <textarea placeholder="Enter First Name" name="fname" ></textarea>
+    <textarea placeholder="Enter First Name" name="fname" id="fname" ></textarea>
     <textarea placeholder="Enter Last Name" name="lname"></textarea>
     <textarea placeholder="Enter street address" name="address"></textarea>
     <textarea placeholder="Enter postal code" name="pcode"></textarea>
     <textarea placeholder="Enter state" name="state"></textarea>
     <textarea placeholder="Enter country" name="country"></textarea>
-
     <input type="submit" value="Proceed to confirmation page">
 
     </form>
-</div>
+    </div>
 
 </body>
 
--- web/shopping-cart/confirm.php
+++ web/shopping-cart/confirm.php
@@ -3,28 +3,17 @@
 
 <head>
 <link rel="stylesheet" href="style.css">
-
-
 </head>
-
 
 <body>
     <!-- adds the navigation bar -->
     <?php include "populate.php" ?>
 
-
     <div class="buffer">
     <h1>Confirmation Page</h1>    
     </div>    
 
-    <?php
-    echo $_POST['fname'];
-    echo "hello world";
-    ?>
-
-    <div class="container"> <p> First Name:  <?php echo $_POST['fname'];  ?> </p> </div>
-    
-
+    <?php echo $_POST['fname']; echo "hello world"; ?>
 </body>
 
 
