f21d5cd11b3937f639db227958d7902e356ebb53
proton5 <jessehillman7@hotmail.com>
2019-01-24 17:18:17 -0700

    fixing navigation to cart page

--- web/shopping-cart/cart.php
+++ web/shopping-cart/cart.php
@@ -13,8 +13,25 @@
 
 
 <?php
-        // update the session
-        include "remove-items.php";
+ 
+        $removeMe = array();
+        
+        // remove requested items from cart.
+        foreach($_POST["rejecteditems"] as $item)
+        {
+            echo "<p> $item </p>";
+        //echo $item;
+        array_push($removeMe,$item);
+        
+        }
+        
+        
+        foreach($removeMe as $removed)
+        {
+            echo $removed;
+            echo "hello world";
+        }
+        
 ?>
 
     <!-- adds the navigation bar -->
