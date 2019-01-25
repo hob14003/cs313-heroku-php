3fbbc5ead17daa43afc3d5288f37af1b0596bc1c
proton5 <jessehillman7@hotmail.com>
2019-01-24 19:18:53 -0700

    different approach

--- web/shopping-cart/confirm.php
+++ web/shopping-cart/confirm.php
@@ -17,12 +17,14 @@
     <h1>Confirmation Page</h1>    
     </div>    
 
-    <div class="container"> <p> First Name:  <?php echo $_POST["fname"]  ?> </p> </div>
-    <div class="container"> <p> Last Name:  <?php echo $_POST["lname"]  ?> </p> </div>
-    <div class="container"> <p> Street Address: <?php echo $_POST["address"]  ?> </p> </div>
-    <div class="container"> <p> Postal Code: <?php echo $_POST["pcode"]  ?> </p> </div>
-    <div class="container"> <p> State: <?php echo $_POST["state"]  ?> </p> </div>
-    <div class="container"> <p> Country: <?php echo $_POST["country"]  ?> </p> </div> 
+    
+
+    <div class="container"> <p> First Name:  <?php echo $_POST["fname"];  ?> </p> </div>
+    <div class="container"> <p> Last Name:  <?php echo $_POST["lname"];  ?> </p> </div>
+    <div class="container"> <p> Street Address: <?php echo $_POST["address"];  ?> </p> </div>
+    <div class="container"> <p> Postal Code: <?php echo $_POST["pcode"];  ?> </p> </div>
+    <div class="container"> <p> State: <?php echo $_POST["state"];  ?> </p> </div>
+    <div class="container"> <p> Country: <?php echo $_POST["country"];  ?> </p> </div> 
 
 
 </body>
