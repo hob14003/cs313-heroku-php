329e013a30dffdd0fdf6607eacc85474e5975c63
proton5 <jessehillman7@hotmail.com>
2019-01-24 18:17:21 -0700

    different approach

--- web/shopping-cart/removeitems.php
+++ web/shopping-cart/removeitems.php
@@ -17,7 +17,31 @@ foreach($removedItems as $it)
 }
 
 
+// check if there is a cart already
+// if there is
+if (array_key_exists('cartItems',$_SESSION) && !empty($_SESSION['cartItems']))
+{
+    $trueCart = $_SESSION['cartItems'];
 
+    foreach($removedItems as $it)
+    {
+
+    // add each new item into the session cart
+    foreach($trueCart as $possiblePurchases)
+    {
+
+        if ($it === $possiblePurchases)
+        {
+            unset($trueCart[$possiblePurchases]);
+        }
+
+    }
+
+    $_SESSION['cartItems'] = $trueCart;
+
+}
+
+}
 
 
 
