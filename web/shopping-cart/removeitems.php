0024006c4e42df5f93460947169b50f42ed50545
proton5 <jessehillman7@hotmail.com>
2019-01-24 17:53:19 -0700

    different approach

--- web/shopping-cart/removeitems.php
+++ web/shopping-cart/removeitems.php
@@ -1,10 +1,41 @@
 <?php session_start(); ?>
 
 <?php
+
+$removedItems = array();
+
+
 foreach($_POST['rejecteditems'] as $yui)
 {
 echo $yui;
+//echo $item;
+array_push($removedItems,$item);
 }
+
+
+// check if there is a cart already
+// if there is
+if (array_key_exists('cartItems',$_SESSION) && !empty($_SESSION['cartItems']))
+{
+    foreach($removedItems as $remove)
+    {
+
+    
+        // add each new item into the session cart
+        foreach($cartItems as $possiblePurchases)
+        {
+            if ($remove == $possiblePurchases)
+            {
+                unset($cartItems[$possiblePurchases]);
+            }
+
+        }
+
+    }
+
+}
+
+
 
 
 ?>
