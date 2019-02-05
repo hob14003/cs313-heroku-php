1233e72895779345503456bda510d3ea66e8ce9a
proton5 <jessehillman7@hotmail.com>
2019-02-05 11:29:51 -0700

    added prepare05

--- web/prepare-05/prepare.php
+++ web/prepare-05/prepare.php
@@ -16,17 +16,25 @@ try
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
-  $id = 1;
-$name = "Jesse";
+//   $id = 1;
+//   $name = "Jesse";
 
 
- $stmt = $db->prepare('SELECT * FROM clients WHERE id=:id AND username=:name');
- $stmt->bindValue(':id', $id, PDO::PARAM_INT);
- $stmt->bindValue(':name', $name, PDO::PARAM_STR);
- $stmt->execute();
- $rows = $stmt->fetchAll(PDO::FETCH_ASSOC); 
+//  $stmt = $db->prepare('SELECT * FROM clients WHERE id=:id AND username=:name');
+//  $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+//  $stmt->bindValue(':name', $name, PDO::PARAM_STR);
+//  $stmt->execute();
+//  $rows = $stmt->fetchAll(PDO::FETCH_ASSOC); 
 
-echo $rows['username'];
+// echo $rows['username'];
+
+
+foreach ($db->query('SELECT username, phone FROM clients') as $row)
+{
+  echo 'user: ' . $row['username'];
+  echo ' password: ' . $row['password'];
+  echo '<br/>';
+}
 
 
 }
