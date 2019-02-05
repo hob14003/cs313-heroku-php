8019d77a6578f18bb71d0cdac9474079bea92506
proton5 <jessehillman7@hotmail.com>
2019-02-05 11:16:43 -0700

    added prepare05

--- web/prepare-05/prepare.php
+++ web/prepare-05/prepare.php
@@ -21,15 +21,15 @@ catch (PDOException $ex)
   die();
 }
 
-$id = 1;
-$name = "jesse";
+// $id = 1;
+// $name = "jesse";
 
 
-$stmt = $db->prepare('SELECT * FROM employees WHERE id=:id AND name=:name');
-$stmt->bindValue(':id', $id, PDO::PARAM_INT);
-$stmt->bindValue(':name', $name, PDO::PARAM_STR);
-$stmt->execute();
-$rows = $stmt->fetchAll(PDO::FETCH_ASSOC); 
+// $stmt = $db->prepare('SELECT * FROM employees WHERE id=:id AND name=:name');
+// $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+// $stmt->bindValue(':name', $name, PDO::PARAM_STR);
+// $stmt->execute();
+// $rows = $stmt->fetchAll(PDO::FETCH_ASSOC); 
 
 
 ?>
