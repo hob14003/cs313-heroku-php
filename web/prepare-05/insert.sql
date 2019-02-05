1039f5a90a3b79788d29438f5d9eaaa8ec9ea293
proton5 <jessehillman7@hotmail.com>
2019-02-05 11:14:17 -0700

    added prepare05

---
+++ web/prepare-05/insert.sql
@@ -0,0 +1,14 @@
+-- Clients
+
+INSERT INTO CLIENTS (username,phone,about)
+VALUES ('Jesse','2083603811','Boss');
+
+
+-- employees
+INSERT INTO Employees (username,phone,position,email,startdate,enddate)
+VALUES ('Monica','2083608987','Accountant','accountant@gmail.com','1/1/2019','1/1/2020');
+
+
+-- calls
+
+
---
+++ web/prepare-05/prepare.php
@@ -0,0 +1,31 @@
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+$id = 1;
+$name = "jesse";
+
+
+$stmt = $db->prepare('SELECT * FROM employees WHERE id=:id AND name=:name');
+$stmt->bindValue(':id', $id, PDO::PARAM_INT);
+$stmt->bindValue(':name', $name, PDO::PARAM_STR);
+$stmt->execute();
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
