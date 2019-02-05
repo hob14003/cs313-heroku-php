78496542fc1ab0d21b19932686cac65d507a0b5f
proton5 <jessehillman7@hotmail.com>
2019-02-05 11:15:59 -0700

    added prepare05

--- web/prepare-05/prepare.php
+++ web/prepare-05/prepare.php
@@ -1,3 +1,4 @@
+<?php
 try
 {
   $dbUrl = getenv('DATABASE_URL');
@@ -28,4 +29,7 @@ $stmt = $db->prepare('SELECT * FROM employees WHERE id=:id AND name=:name');
 $stmt->bindValue(':id', $id, PDO::PARAM_INT);
 $stmt->bindValue(':name', $name, PDO::PARAM_STR);
 $stmt->execute();
-$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC); 
+
+
+?>
