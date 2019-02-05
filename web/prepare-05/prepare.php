5ae2e65ec03ff688e7bcf1ddde272c5a95136b4b
proton5 <jessehillman7@hotmail.com>
2019-02-05 11:21:14 -0700

    added prepare05

--- web/prepare-05/prepare.php
+++ web/prepare-05/prepare.php
@@ -14,14 +14,9 @@ try
   $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $ex)
-{
-  echo 'Error!: ' . $ex->getMessage();
-  die();
-}
 
-$id = 1;
+
+  $id = 1;
 $name = "jesse";
 
 
@@ -29,7 +24,19 @@ $name = "jesse";
  $stmt->bindValue(':id', $id, PDO::PARAM_INT);
  $stmt->bindValue(':name', $name, PDO::PARAM_STR);
  $stmt->execute();
-// $rows = $stmt->fetchAll(PDO::FETCH_ASSOC); 
+ $rows = $stmt->fetchAll(PDO::FETCH_ASSOC); 
+
+
+
+
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+
 
 
 ?>
