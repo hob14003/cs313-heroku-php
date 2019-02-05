1eb64313bcb1310a37ffd129b8e92d4b5cf5d0ed
proton5 <jessehillman7@hotmail.com>
2019-02-05 11:22:12 -0700

    added prepare05

--- web/prepare-05/prepare.php
+++ web/prepare-05/prepare.php
@@ -20,7 +20,7 @@ try
 $name = "jesse";
 
 
- $stmt = $db->prepare('SELECT * FROM employees WHERE id=:id AND name=:name');
+ $stmt = $db->prepare('SELECT * FROM employees WHERE id=:id AND username=:name');
  $stmt->bindValue(':id', $id, PDO::PARAM_INT);
  $stmt->bindValue(':name', $name, PDO::PARAM_STR);
  $stmt->execute();
