a79a99aa132e8591477bacef6bcb26980fb9c034
proton5 <jessehillman7@hotmail.com>
2019-02-07 17:14:28 -0700

    Added the client manager phone book

--- web/phone-book/script.js
+++ web/phone-book/script.js
@@ -1,18 +1,25 @@
-// function updateList(client)
-// {
-//     console.log(client);
-//     console.log(client.value);
-// }
-
 let searchElement = document.getElementById("query");
 console.log(searchElement);
 searchElement.addEventListener("keyup",(client) => {
 
-    
+    // for whether its internet explorer or other browsers
     var target = (client.target) ? client.target : client.srcElement;
     console.log(target.value);
 
-}
+    // fetch the list
+    fetch(select-clients.php, {
+        method: 'Post',
+        body: dbData
+    })
+    .then(res => res.text()).then(text => console.log(text))
+    .catch(error => console.error('DB error: ', error));
 
 
-);
+
+
+
+
+
+
+// end of function
+});
---
+++ web/phone-book/select-clients.php
@@ -0,0 +1,45 @@
+<?php
+try
+{
+    // hold the client name passed in;
+ //  $client = $_POST["client"];
+
+ //  echo $client;
+
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+
+
+
+foreach ($db->query('SELECT username, phone FROM clients') as $row)
+{
+  echo 'user: ' . $row['username'];
+  echo ' phone number: ' . $row['phone'];
+  echo '<br/>';
+}
+
+
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+
+
+
+?>
