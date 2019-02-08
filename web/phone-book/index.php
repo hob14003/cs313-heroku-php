2092ef38ba2595b69ee6977919c45d43af233423
proton5 <jessehillman7@hotmail.com>
2019-02-07 19:43:30 -0700

    Added the client manager phone book

--- web/phone-book/index.php
+++ web/phone-book/index.php
@@ -17,9 +17,9 @@
 <input type="search" placeholder = "Enter Client Name" name="query" id="query">
 <div id="clientlist">
 
-<div id="results" class="container">
+    <div id="results" class="form">
     
-</div>
+    </div>
 
 
 </div>
