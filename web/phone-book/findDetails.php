385c157b5a8d29b5b197608cbaaf8a8428000e3e
proton5 <jessehillman7@hotmail.com>
2019-02-07 21:16:11 -0700

    Added the client manager phone book

--- web/phone-book/findDetails.php
+++ web/phone-book/findDetails.php
@@ -30,7 +30,7 @@ echo "<div class='container'>";
      $phoneNumber = $row['phone'];
      $position = $row['about'];
 
-   echo "<p>"
+ //  echo "<p>"
    echo "Client name is ";
    echo $username;
   // echo "\n";
@@ -43,7 +43,7 @@ echo "<div class='container'>";
    echo $position;
   // echo "\n";
 
-   echo "</p>";
+ //  echo "</p>";
 
 
  }
