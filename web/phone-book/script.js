ac8519d103f94c7860c2412c13ef4ffb08b3e590
proton5 <jessehillman7@hotmail.com>
2019-02-07 18:17:24 -0700

    Added the client manager phone book

--- web/phone-book/script.js
+++ web/phone-book/script.js
@@ -1,15 +1,43 @@
 let searchElement = document.getElementById("query");
 console.log(searchElement);
+
+
+
+/******************
+ * 
+ * 
+ * 
+ * ********* */
+function populateResults(clients)
+{
+    console.log(clients);
+
+    let results = document.getElementById("results");
+
+    // create a new button
+    let newButton = document.createElement("BUTTON");
+    newButton.value = clients; 
+
+    // add the new button
+    results.appendChild(newButton);
+
+}
+
+
+
+
+/*********************
+ * Author: Jesse Hillman
+ * Query the database looking for
+ * someone with that name
+ * ************/
 searchElement.addEventListener("keyup",(client) => {
 
     // for whether its internet explorer or other browsers
     var target = (client.target) ? client.target : client.srcElement;
-  //  console.log(target.value);
 
     // holds the value to be selected
     let dbData = target.value;
-
-    
 
     // fetch the list
     fetch("select-clients.php", {
@@ -17,15 +45,7 @@ searchElement.addEventListener("keyup",(client) => {
         body: 'client=' + dbData,
         headers: { 'Content-type': 'application/x-www-form-urlencoded' }
     })
-    .then(res => res.text()).then(text => console.log(text))
+    .then(res => res.text()).then(text => populateResults(text))
     .catch(error => console.error('DB error: ', error));
-
-
-
-
-
-
-
-
 // end of function
 });
