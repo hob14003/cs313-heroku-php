046571d5ae9cc167f36fa51e543aa95f0f36be85
proton5 <jessehillman7@hotmail.com>
2019-02-12 21:36:15 -0700

    added the team activity

--- web/team-activity-6/query.php
+++ web/team-activity-6/query.php
@@ -51,7 +51,6 @@ try
 
 
     foreach ($topicIds as $topicId)
-
 	{
 
 		echo "ScriptureId: $scriptureId, topicId: $topicId";
@@ -74,7 +73,68 @@ try
 
 		$statement->execute();
 
+    }
+    
+    $statement = $db->prepare('SELECT id, book, chapter, verse, content FROM scripture');
+
+	$statement->execute();
+
+
+
+	// Go through each result
+
+	while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+
+	{
+
+		echo '<p>';
+
+		echo '<strong>' . $row['book'] . ' ' . $row['chapter'] . ':';
+
+		echo $row['verse'] . '</strong>' . ' - ' . $row['content'];
+
+		echo '<br />';
+
+		echo 'Topics: ';
+
+
+
+		// get the topics now for this scripture
+
+		$stmtTopics = $db->prepare('SELECT name FROM topic t'
+
+			. ' INNER JOIN scripture_topic st ON st.topicId = t.id'
+
+			. ' WHERE st.scriptureId = :scriptureId');
+
+
+
+		$stmtTopics->bindValue(':scriptureId', $row['id']);
+
+		$stmtTopics->execute();
+
+
+
+		// Go through each topic in the result
+
+		while ($topicRow = $stmtTopics->fetch(PDO::FETCH_ASSOC))
+
+		{
+
+			echo $topicRow['name'] . ' ';
+
+		}
+
+
+
+		echo '</p>';
+
 	}
+
+
+
+
+
 
 
 }
