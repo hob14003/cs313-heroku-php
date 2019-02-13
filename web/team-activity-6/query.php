59808cfb12a68fc783b8cc64ceba4bac52258f2c
proton5 <jessehillman7@hotmail.com>
2019-02-12 21:07:48 -0700

    added the team activity

---
+++ web/team-activity-6/query.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+// get the data from the POST
+
+$book = $_POST['txtBook'];
+
+$chapter = $_POST['txtChapter'];
+
+$verse = $_POST['txtVerse'];
+
+$content = $_POST['txtContent'];
+
+$topicIds = $_POST['chkTopics'];
+
+
+
+echo "book=$book\n";
+
+
+?>
