1b04f1719c801362070d383beea326a2c1196911
proton5 <jessehillman7@hotmail.com>
2019-02-19 13:44:37 -0700

    added team activity 7

--- web/team-activity-7/signup.php
+++ web/team-activity-7/signup.php
@@ -8,14 +8,14 @@
 
 <body>
 
-
+<div class="container">
 <form action="addClient.php" method="post">
 <h1>Enter Login Information</h1>
 <input type="text" name="Name" placeholder="Enter Username">
 <input type="text" name="Password" placeholder="Enter Password">
 <input type="submit" value="Sign Up">
 </form>
-
+</div>
 
 </body>
 
