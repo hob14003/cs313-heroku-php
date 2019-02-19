9986ab4964f5251cdafdb65a7dc07f4057f16171
proton5 <jessehillman7@hotmail.com>
2019-02-19 15:53:30 -0700

    added team activity 7

--- web/team-activity-7/script.js
+++ web/team-activity-7/script.js
@@ -1,6 +1,3 @@
-let searchElement = document.getElementById("query");
-
-
 /**********
  * Author:Jesse Hillman
  * 
@@ -14,21 +11,32 @@ function verifyLogin()
    let username = document.getElementById("name").value;
    let password = document.getElementById("password").value;
 
-   alert(username);
-   alert(password);
+//   alert(username);
+//   alert(password);
 
    // fetch the list
-//    fetch("findDetails.php", {
-//       method: 'Post',
-//       body: 'client=' + client,
-//       headers: { 'Content-type': 'application/x-www-form-urlencoded' }
-//   })
-//   .then(res => res.text()).then(text => createDetailView(text))
-//   .catch(error => console.error('DB error: ', error));
+   fetch("findDetails.php", {
+      method: 'Post',
+      body: 'username=' + username + '&' + 'password=' + password,
+      headers: { 'Content-type': 'application/x-www-form-urlencoded' }
+  })
+  .then(res => res.text()).then(text => createDetailView(text))
+  .catch(error => console.error('DB error: ', error));
+
+
+}
 
 
 
-
+/**********
+ * Author: Jesse Hillman
+ * 
+ * 
+ * Purpose: Create the message
+ * *************** */
+function createDetailView(text)
+{
+   alert("we are in create View");
 
 }
 
