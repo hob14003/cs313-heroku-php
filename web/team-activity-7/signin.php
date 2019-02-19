6aa9e3e4f05b6e2338e7ba0e6ccedfd0ddb403c3
proton5 <jessehillman7@hotmail.com>
2019-02-19 15:35:53 -0700

    added team activity 7

---
+++ web/team-activity-7/signin.php
@@ -0,0 +1,25 @@
+<html>
+
+<head>
+<link rel="stylesheet" href="style.css">
+<script src="script.js" defer> </script>
+</head>
+
+
+<body>
+
+<div class="container">
+<form action="addNewUser.php" method="post">
+<h1>Enter Login Information</h1>
+<input type="text" name="Name" placeholder="Enter Username">
+<input type="password" name="Password" placeholder="Enter Password">
+<input type="submit" value="Log in">
+</form>
+</div>
+
+</body>
+
+
+
+
+</html>
