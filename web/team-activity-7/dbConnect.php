8c8d9cc03e881cfcfd0c86ae523fac7fc2a94ec5
proton5 <jessehillman7@hotmail.com>
2019-02-19 19:41:08 -0700

    added team activity 7

--- web/team-activity-7/dbConnect.php
+++ web/team-activity-7/dbConnect.php
@@ -1,16 +1,113 @@
 <?php
-  $dbUrl = getenv('DATABASE_URL');
 
-  $dbOpts = parse_url($dbUrl);
+/**********************************************************
 
-  $dbHost = "ec2-107-20-167-11.compute-1.amazonaws.com";
-  $dbPort = "5432";
-  $dbUser = "eeoypxkfqceele";
-  $dbPassword = "8a8e822c95e1d6a4a2215c88ac4b20bc89f84ae1cc7480ce7212d82d1ba707e0";
-  $dbName = "df3hvlpt6i3j3h";
+* File: dbConnect.php
 
-  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+* Author: Br. Burton
+
+* 
+
+* Description: Shows how to connect using either local
+
+* OR Heroku credentials, depending on whether the code
+
+* is executing at heroku.
+
+***********************************************************/
 
 
-  ?>
+
+function get_db() {
+
+	$db = NULL;
+
+
+
+	try {
+
+		// default Heroku Postgres configuration URL
+
+		$dbUrl = getenv('DATABASE_URL');
+
+
+
+		if (!isset($dbUrl) || empty($dbUrl)) {
+
+			// example localhost configuration URL with user: "ta_user", password: "ta_pass"
+
+			// and a database called "scripture_ta"
+
+			$dbUrl = "postgres://ta_user:ta_pass@localhost:5432/login_test";
+
+
+
+			// NOTE: It is not great to put this sensitive information right
+
+			// here in a file that gets committed to version control. It's not
+
+			// as bad as putting your Heroku user and password here, but still
+
+			// not ideal.
+
+			
+
+			// It would be better to put your local connection information
+
+			// into an environment variable on your local computer. That way
+
+			// it would work consistently regardless of whether the application
+
+			// were running locally or at heroku.
+
+		}
+
+
+
+		// Get the various parts of the DB Connection from the URL
+
+		$dbopts = parse_url($dbUrl);
+
+
+
+		$dbHost = $dbopts["host"];
+
+		$dbPort = $dbopts["port"];
+
+		$dbUser = $dbopts["user"];
+
+		$dbPassword = $dbopts["pass"];
+
+		$dbName = ltrim($dbopts["path"],'/');
+
+
+
+		// Create the PDO connection
+
+		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+
+
+		// this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
+
+		$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+
+	}
+
+	catch (PDOException $ex) {
+
+		// If this were in production, you would not want to echo
+
+		// the details of the exception.
+
+		echo "Error connecting to DB. Details: $ex";
+
+		die();
+
+	}
+
+
+
+	return $db;
+
+}
