a0e22016ef26999a21c1486ace6ef84f524490bc
proton5 <jessehillman7@hotmail.com>
2019-02-19 17:54:18 -0700

    added team activity 7

--- web/team-activity-7/verify.php
+++ web/team-activity-7/verify.php
@@ -4,24 +4,24 @@ try
     // hold the client name passed in;
    $username = $_POST["Name"];
    $password = $_POST["Password"];
-//    $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+   $passwordHash = password_hash($password, PASSWORD_DEFAULT);
 
-  $dbUrl = getenv('DATABASE_URL');
+//   $dbUrl = getenv('DATABASE_URL');
 
-  $dbOpts = parse_url($dbUrl);
+//   $dbOpts = parse_url($dbUrl);
 
-  $dbHost = $dbOpts["host"];
-  $dbPort = $dbOpts["port"];
-  $dbUser = $dbOpts["user"];
-  $dbPassword = $dbOpts["pass"];
-  $dbName = ltrim($dbOpts["path"],'/');
+//   $dbHost = $dbOpts["host"];
+//   $dbPort = $dbOpts["port"];
+//   $dbUser = $dbOpts["user"];
+//   $dbPassword = $dbOpts["pass"];
+//   $dbName = ltrim($dbOpts["path"],'/');
 
-  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+//   $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
   echo "made it this far";
-
+   echo $passwordHash;
 
 //  foreach ($db->query('SELECT username, password FROM auth WHERE username' . '=' . $username . ' AND ' . 'password=' . $passwordHash . ';'  ) as $row)
 //  {
