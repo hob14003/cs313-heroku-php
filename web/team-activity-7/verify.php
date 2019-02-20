7aa8dc15385a05a4b01482a9214bdd4ef30dc3fe
proton5 <jessehillman7@hotmail.com>
2019-02-19 17:48:57 -0700

    added team activity 7

--- web/team-activity-7/verify.php
+++ web/team-activity-7/verify.php
@@ -1,62 +1,61 @@
 <?php
-// try
-// {
-//     // hold the client name passed in;
-//    $username = $_POST["Name"];
-//    $password = $_POST["Password"];
-//    $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+try
+{
+    // hold the client name passed in;
+   $username = $_POST["Name"];
+   $password = $_POST["Password"];
+   $passwordHash = password_hash($password, PASSWORD_DEFAULT);
 
-//     echo "made it this far";
+    echo "made it this far";
 
-//   $dbUrl = getenv('DATABASE_URL');
+  $dbUrl = getenv('DATABASE_URL');
 
-//   $dbOpts = parse_url($dbUrl);
+  $dbOpts = parse_url($dbUrl);
 
-//   $dbHost = $dbOpts["host"];
-//   $dbPort = $dbOpts["port"];
-//   $dbUser = $dbOpts["user"];
-//   $dbPassword = $dbOpts["pass"];
-//   $dbName = ltrim($dbOpts["path"],'/');
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
 
-//   $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-//   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
-//  foreach ($db->query('SELECT username, password FROM auth WHERE username' . '=' . $username . ' AND ' . 'password=' . $passwordHash . ';'  ) as $row)
-//  {
+ foreach ($db->query('SELECT username, password FROM auth WHERE username' . '=' . $username . ' AND ' . 'password=' . $passwordHash . ';'  ) as $row)
+ {
 
   
 
-//      $username =  $row['username'];
-//      $password =  $row['password'];
+     $username =  $row['username'];
+     $password =  $row['password'];
 
 
-//    echo "Hash is ";
-//    echo $username;
-
-
-
-//    echo "Password is ";
-//    echo $password;
+   echo "Hash is ";
+   echo $username;
 
 
 
-
-//  }
+   echo "Password is ";
+   echo $password;
 
 
 
 
-// }
-// catch (PDOException $ex)
-// {
-//   echo 'Error!: ' . $ex->getMessage();
-//   die();
-// }
+ }
 
 
-echo "Hello world";
+
+
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+
 
 ?>
 
