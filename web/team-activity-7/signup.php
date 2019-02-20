669be831942466659286edc3f78a48dd1f340cf7
proton5 <jessehillman7@hotmail.com>
2019-02-19 19:46:13 -0700

    added team activity 7

---
+++ web/team-activity-7/signup.php
@@ -0,0 +1,25 @@
+<html>
+
+<head>
+<link rel="stylesheet" href="style.css">
+<script src="script.js" defer> </script>
+</head>
+
+
+<body>
+
+<div class="container">
+<form action="addNewUser.php" method="post">
+<h1>Sign up for a account</h1>
+<input type="text" name="Name" placeholder="Enter Username">
+<input type="text" name="Password" placeholder="Enter Password">
+<input type="submit" value="Sign Up">
+</form>
+</div>
+
+</body>
+
+
+
+
+</html>
---
+++ web/team-activity-7/verify.php
@@ -0,0 +1,53 @@
+<?php
+try
+{
+    // hold the client name passed in;
+   $username = $_POST['username'];
+ //  $password = $_POST["password"];
+ //  $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  echo "made it this far";
+
+
+//foreach ($db->query('SELECT username, password FROM auth WHERE username' . '=' . '\''. $username . '\''  . ';'  ) as $row)
+//foreach ($db->query('SELECT username FROM auth WHERE username LIKE' . '\'' . $username . '%\'') as $row)
+foreach ($db->query('SELECT * FROM auth WHERE username' . '=' . '\'' . $username . '%\'') as $row)
+{
+     $results =  $row['username'];
+   //   $password = $row['password'];
+
+   echo "Hash is ";
+   echo $results;
+ //  echo $password;
+
+}
+
+
+
+
+
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+ // die();
+}
+
+
+
+?>
+
